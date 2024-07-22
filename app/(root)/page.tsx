9962df719bab4002613ed1914c932149e0a4d750
Uche Anota <ucheobi2009@yahoo.com>
2024-07-22 11:01:52 +0200

    implemented authentication with appwrite

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,10 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = () => {
-  const loggedIn = {
-    firstName: "Uche",
-    lastName: "Anota",
-    email: "uche.anota@yahoo.com",
-  };
-
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
   return (
     <section className="home">
       <div className="home-content">
@@ -16,7 +12,7 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || "Guest"}
+            user={loggedIn?.name || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 
 const Sidebar = ({ user }: SidebarProps) => {
   const pathname = usePathname();
@@ -53,7 +54,7 @@ const Sidebar = ({ user }: SidebarProps) => {
         })}
         User
       </nav>
-      Footer
+      <Footer user={user} />
     </section>
   );
 };
